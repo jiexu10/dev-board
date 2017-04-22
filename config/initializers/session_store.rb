967f6e1a4d8ef7559c0180983d11f3ad1b9775b8
Jie Xu <jie.xu.10@gmail.com>
2017-04-22 12:15:56 -0400

    rename repo and references

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_adding-react-to-rails_session'
+Rails.application.config.session_store :cookie_store, key: '_dev_board_session'
