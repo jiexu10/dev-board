ad20ceb280c8a5f19e19c5f3dd939e0d56d672ad
Jie Xu <jie.xu.10@gmail.com>
2017-05-05 18:19:09 -0400

    add gh stylesheet and basic styling for open PRs

--- react/src/components/ghEventListItem.jsx
+++ react/src/components/ghEventListItem.jsx
@@ -7,7 +7,7 @@ export default class GhEventListItem extends React.Component {
 
   render(){
     return (
-      <li key={this.props.data.name} className='accordion-item' data-accordion-item>
+      <li key={this.props.data.name} className={'accordion-item ' + this.props.data.state} data-accordion-item>
         <a href='#' className='accordion-title'>{this.props.data.action} {this.props.data.title}</a>
         <div className='accordion-content' data-tab-content>
           <p>some thing here</p>
