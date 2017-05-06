c63e81e4a5a25227ab5eae88e1f070b2ffdd4bcf
Jie Xu <jie.xu.10@gmail.com>
2017-05-06 17:38:32 -0400

    add foundation-icons-3

--- react/src/components/ghEventListItem.jsx
+++ react/src/components/ghEventListItem.jsx
@@ -8,7 +8,15 @@ export default class GhEventListItem extends React.Component {
   render(){
     return (
       <li key={this.props.data.name} className={'accordion-item ' + this.props.data.state} data-accordion-item>
-        <a href='#' className='accordion-title'>{this.props.data.action} {this.props.data.title}</a>
+        <a href='#' className='accordion-title'>
+          <div>
+            {this.props.data.action} {this.props.data.title}
+          </div>
+          <div>
+            <i className='fi-comments' />
+            {this.props.data.comments}
+          </div>
+        </a>
         <div className='accordion-content' data-tab-content>
           <p>some thing here</p>
         </div>
