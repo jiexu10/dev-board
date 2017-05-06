939fff6bd71c084c26e8d92dbfd987a89d0ddf34
Jie Xu <jie.xu.10@gmail.com>
2017-05-06 15:00:52 -0400

    fix update method to write to the existing db entry

--- app/controllers/api/v1/gh_payloads_controller.rb
+++ app/controllers/api/v1/gh_payloads_controller.rb
@@ -8,9 +8,9 @@ class Api::V1::GhPayloadsController < ApplicationController
     firebase = Firebase::Client.new(ENV['FIREBASE_URL'])
     if request.env['HTTP_X_GITHUB_EVENT'] == 'pull_request'
       serialized_params = Firebase::Github::PullRequestSerializer.new(push).params
-      existing_event = firebase.get('gh_events', id: serialized_params[:id])
-      if existing_event.present?
-        firebase.update('gh_events', serialized_params)
+      existing_event = firebase.get('gh_events', { orderBy: '"id"', equalTo: serialized_params[:id] }).body
+      if existing_event.keys.any?
+        firebase.update("gh_events/#{existing_event.keys.first}", serialized_params)
       else
         firebase.push('gh_events', serialized_params)
       end
